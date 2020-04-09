d526c29c9ca992e247a0c093a5837b83b73d46e1
Balal Naeem <balalnaeem@gmail.com>
2020-04-09 17:03:12 +0100

    strings in JS

---
+++ lesson_2/practice.js
@@ -0,0 +1,25 @@
+let firstName = 'Balal';
+let lastName = 'Naeem';
+
+let fullName = firstName + ' ' + lastName;
+
+let language = 'JavaScript';
+let idx = language.indexOf('S');
+let charCode = language.charCodeAt(idx);
+
+let a = 'a';
+let b = 'b';
+
+
+let aIndex = language.indexOf('a');
+let vIndex = language.indexOf('v');
+
+let fact1 = 'JavaScript is fun';
+let fact2 = 'Kids like it too';
+
+let compoundSentence = fact1 + ' and ' + fact2.toLowerCase();
+
+let pi = 22 / 7;
+pi = pi.toString();
+
+console.log(pi.lastIndexOf('14'));
